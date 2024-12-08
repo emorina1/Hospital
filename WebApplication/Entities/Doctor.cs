55c7ce1f705a755f964a7c707fa630a084007946
emorina1 <emorina1@example.com>
2024-12-08 17:14:37 +0100

    Add project files.

---
+++ WebApplication/Entities/Doctor.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication.Entities
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //Lista e Pacientave
+
+        public string Departamenti { get; set; }
+
+        public DateTime Termini { get; set; }
+
+        public string CoverImage { get; set; }
+    }
+}
