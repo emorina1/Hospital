d0f6fe672c196555b2ba836040526c1cc7527085
emorina1 <emorina1@example.com>
2024-12-08 17:47:26 +0100

    add pacient

--- WebApplication/Entities/Doctor.cs
+++ WebApplication/Entities/Doctor.cs
@@ -1,17 +1,41 @@
-﻿namespace WebApplication.Entities
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Entities
 {
+    public enum Department
+    {
+        Cardiology,
+        Neurology,
+        Pediatrics,
+        GeneralPractice
+    }
+
     public class Doctor
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
         public string Name { get; set; }
+
+        [StringLength(500)]
         public string Description { get; set; }
 
-        //Lista e Pacientave
+        public ICollection<Pacient> Pacients { get; set; }
 
-        public string Departamenti { get; set; }
+        public Department Departamenti { get; set; }
 
+        // Ose orari i punës
         public DateTime Termini { get; set; }
 
         public string CoverImage { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+        public DateTime? ModifiedDate { get; set; }
     }
 }
---
+++ WebApplication/Entities/Pacient.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Entities
+{
+    public class Pacient
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }  
+
+        public DateTime DateOfBirth { get; set; }
+
+        public ICollection<Doctor> Doctors{ get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+        public DateTime? ModifiedDate { get; set; }
+ 
+
+    }
+}
